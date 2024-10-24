fc37af2f805e20c0e512a862c4f2d707f27b50f9
to0mas <rehanektomas123@gmail.com>
2024-10-24 17:09:00 +0200

    commit

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+gsap.from(".nadpis", { opacity: 0, duration: 2, ease: "power1.inOut" });
+
+gsap.from(".podnadpis", { opacity: 0, duration:7, ease: "power1.inOut" });
